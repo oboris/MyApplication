a30464a4b262adafd6721c79f3b04da056ba7bf7
Onyshchenko Borys <onischenkoboris@gmail.com>
2017-11-22 23:41:00 +0200

    added fab behavior custom class

---
+++ app/src/main/java/com/example/user/myapplication/util/FABFloatOnScroll.java
@@ -0,0 +1,35 @@
+package com.example.user.myapplication.util;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+public class FABFloatOnScroll extends FloatingActionButton.Behavior {
+
+    public FABFloatOnScroll(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+        if (dyConsumed > 0) {
+            CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+            int fabBottomMargin = layoutParams.bottomMargin;
+            child.animate().translationY(child.getHeight() + fabBottomMargin).setInterpolator(new LinearInterpolator()).start();
+        } else if (dyConsumed < 0) {
+            child.animate().translationY(0).setInterpolator(new LinearInterpolator()).start();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
+
+    }
+}
