39ecd2308f8507125f2c4cb529a54295cc2deac3
Onyshchenko Borys <onischenkoboris@gmail.com>
2017-11-22 09:19:17 +0200

    next step 1

--- app/src/main/java/com/example/user/myapplication/dao/MotherBoardDao.java
+++ app/src/main/java/com/example/user/myapplication/dao/MotherBoardDao.java
@@ -1,6 +1,15 @@
 package com.example.user.myapplication.dao;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.user.myapplication.database.MultiDbHelper;
+import com.example.user.myapplication.model.MotherBoard;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MotherBoardDao {
     private Context context;
@@ -35,5 +44,67 @@ public class MotherBoardDao {
 
     public MotherBoardDao(Context context) {
         this.context = context;
+    }
+
+    public synchronized List<MotherBoard> selectAllMotherBoardFromDB() {
+        final SQLiteDatabase db = MultiDbHelper.getInstance(context).getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME_MB, null,
+                null, null, null, null, null);
+
+        List<MotherBoard> motherBoards = convertFromCursorToListMBs(cursor);
+        cursor.close();
+
+        for (MotherBoard motherBoard: motherBoards) {
+            cursor = db.query(TABLE_NAME_MB_PICT, null,
+                    COLUMN_ID_MB + "= ?", new String[]{String.valueOf(motherBoard.getIdMotherBoard())},
+                    null, null, null);
+            List<String> picUrls = convertFromCursorToListPicUrls(cursor);
+            motherBoard.setPhotos(picUrls);
+        }
+        cursor.close();
+
+        db.close();
+        return motherBoards;
+    }
+
+    private List<MotherBoard> convertFromCursorToListMBs(Cursor cursor) {
+        List<MotherBoard> motherBoards = new ArrayList<>(cursor.getCount());
+        cursor.moveToFirst();
+        if (!cursor.isAfterLast()) {
+            do {
+                motherBoards.add(fromCursorToMB(cursor));
+            } while (cursor.moveToNext());
+        }
+        return motherBoards;
+    }
+
+   private MotherBoard fromCursorToMB(Cursor cursor) {
+        int idMB = cursor.getInt(cursor.getColumnIndex(COLUMN_ID_MB));
+        String name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
+        String chipSet = cursor.getString(cursor.getColumnIndex(COLUMN_CHIP_SET));
+        return new MotherBoard(idMB, name, chipSet);
+    }
+
+    private List<String> convertFromCursorToListPicUrls(Cursor cursor) {
+        List<String> pictUrls = new ArrayList<>(cursor.getCount());
+        cursor.moveToFirst();
+        if (!cursor.isAfterLast()) {
+            do {
+                pictUrls.add(fromCursorToPict(cursor));
+            } while (cursor.moveToNext());
+        }
+        return pictUrls;
+    }
+
+    private String fromCursorToPict(Cursor cursor) {
+        return cursor.getString(cursor.getColumnIndex(COLUMN_PIC_URL));
+    }
+
+    private ContentValues getMBContent(MotherBoard motherBoard) {
+        final ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, motherBoard.getName());
+        values.put(COLUMN_CHIP_SET, motherBoard.getChipSet());
+        return values;
     }
 }
